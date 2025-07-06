6f1481d6fe29c327efdefaf09ed367c2573ca061
Kevin Olivella <kevinolivella@gmail.com>
2025-07-05 22:15:26 -0500

    Se modifica

--- build_exe.py
+++ build_exe.py
@@ -13,6 +13,9 @@ PyInstaller.__main__.run([
     '--name=Gestión Financiera - Garcia Roldan',
     '--add-data=estilos.py;.',  # Incluir archivo de estilos
     '--hidden-import=ttkbootstrap',  # Incluir ttkbootstrap
+    '--hidden-import=ttkbootstrap.themes',  # Incluir temas de ttkbootstrap
+    '--hidden-import=ttkbootstrap.icons',  # Incluir íconos de ttkbootstrap
+    '--hidden-import=matplotlib.backends.backend_tkagg',  # Incluir matplotlib
     '--clean',
     '--distpath', output_dir,  # Especificar carpeta de salida
     '--noconfirm'  # No preguntar por confirmación
